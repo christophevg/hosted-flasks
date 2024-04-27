4f70ccee75ed24ea8e04c860f59ec8f11cebb55c
Christophe VG <contact@christophe.vg>
2024-04-27 15:16:15 +0200

    implemented basic default frontpage

--- hosted_flasks/frontpage.py
+++ hosted_flasks/frontpage.py
@@ -8,18 +8,25 @@ from flask import Flask, render_template
 
 logger = logging.getLogger(__name__)
 
-TEMPLATE_FOLDER = os.environ.get("HOSTED_FLASKS_TEMPLATE_FOLDER", None)
-if not TEMPLATE_FOLDER:
-  TEMPLATE_FOLDER = Path(__file__).resolve().parent / "templates"
-  logger.debug("📰 using default template folder")
+FRONTPAGE_FOLDER = os.environ.get("HOSTED_FLASKS_FRONTPAGE_FOLDER", None)
+if not FRONTPAGE_FOLDER:
+  FRONTPAGE_FOLDER = Path(__file__).resolve().parent / "frontpage"
+  logger.debug("📰 using default frontpage folder")
 else:
-  TEMPLATE_FOLDER = Path(TEMPLATE_FOLDER).resolve()
-  logger.debug(f"📰 using template folder: {TEMPLATE_FOLDER.relative_to(Path.cwd())}")
+  FRONTPAGE_FOLDER = Path(FRONTPAGE_FOLDER).resolve()
+  logger.info(f"📰 using custom frontpage folder: {FRONTPAGE_FOLDER.relative_to(Path.cwd())}")
 
-app = Flask("hosted-flasks", template_folder=TEMPLATE_FOLDER)
+app = Flask(
+  "hosted-flasks",
+  template_folder=FRONTPAGE_FOLDER,
+  static_folder=f"{FRONTPAGE_FOLDER}/static",
+  static_url_path=""
+)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 apps = [] # to be filled externally with apps that are served, see server.py
 
 @app.route("/")
 def show_frontpage():
-  return render_template("frontpage.html", apps=apps)
+  return render_template("index.html", apps=apps)
